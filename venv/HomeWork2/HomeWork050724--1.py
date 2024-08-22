febabce61d80b03c6a4298965753150bd4c29735
doritgy <63942750+doritgy@users.noreply.github.com>
2024-08-22 21:06:52 +0300

    sss

---
+++ venv/HomeWork2/HomeWork050724--1.py
@@ -0,0 +1,10 @@
+##start
+firstNo:int = int(input("please enter the first integer"))
+secondNo:int = int(input("please enter the second integer"))
+thirdNo:int = int(input("please enter the third integer"))
+result:int = firstNo + secondNo + thirdNo
+print("the sum of the three interers is:" , result)
+
+result:int = firstNo * secondNo * thirdNo
+print("the multiplication of the three interers is:" , result)
+##end
